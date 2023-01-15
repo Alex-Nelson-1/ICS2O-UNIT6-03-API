e7336974062fdc4dff128efc06c9976b17ad732f
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-15 13:09:52 +0000

    Api working and information show

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Created by: Alex Nelson
-// Created on: Oct 2022
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -16,6 +16,29 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+// fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+// .then((res) => res.json())
+// .then((data) => console.log(data))
+// .catch((err) => console.error(err))
+
+
+const getWeather = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    if (jsonData.main) {
+      const temp = jsonData.main.temp - 273.15
+      const feel = jsonData.main.feels_like - 273.15
+      const humidity = jsonData.main.humidity
+     document.getElementById("temperature").innerHTML = "Temperature: " + temp.toFixed(0) + " °C" + "<br>" + "Feels Like: " + feel.toFixed(0) + " °C" + "<br>" 
+ + "Humidity: " + humidity.toFixed(0)
+  } else {
+    console.log(error)
+  }
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
