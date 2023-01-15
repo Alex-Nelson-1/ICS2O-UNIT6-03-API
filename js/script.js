8f9d219086c1b5090e11e1960180800d978868e2
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2023-01-15 13:11:47 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ if (navigator.serviceWorker) {
 // .then((data) => console.log(data))
 // .catch((err) => console.error(err))
 
-
 const getWeather = async (URLAddress) => {
   try {
     const result = await fetch(URLAddress)
@@ -31,14 +30,25 @@ const getWeather = async (URLAddress) => {
       const temp = jsonData.main.temp - 273.15
       const feel = jsonData.main.feels_like - 273.15
       const humidity = jsonData.main.humidity
-     document.getElementById("temperature").innerHTML = "Temperature: " + temp.toFixed(0) + " °C" + "<br>" + "Feels Like: " + feel.toFixed(0) + " °C" + "<br>" 
- + "Humidity: " + humidity.toFixed(0)
-  } else {
-    console.log(error)
-  }
+      document.getElementById("temperature").innerHTML =
+        "Temperature: " +
+        temp.toFixed(0) +
+        " °C" +
+        "<br>" +
+        "Feels Like: " +
+        feel.toFixed(0) +
+        " °C" +
+        "<br>" +
+        "Humidity: " +
+        humidity.toFixed(0)
+    } else {
+      console.log(error)
+    }
   } catch (err) {
     console.log(err)
   }
 }
 
-getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
