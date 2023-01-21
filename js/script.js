0bdd2ed3b5a4230129be76359a6aecc62ba6ea49
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-21 10:20:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,46 +14,49 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function displays the weather by the minute.
  */
-// fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-// .then((res) => res.json())
-// .then((data) => console.log(data))
-// .catch((err) => console.error(err))
 
 const getWeather = async (URLAddress) => {
   try {
-    //Fetch
+    // Fetch
     const result = await fetch(URLAddress)
     const jsonData = await result.json()
     console.log(jsonData)
+
+    //Variable for img
+    const icon = jsonData.weather[0].icon
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    if (jsonData.weather[0].id) {
+    document.getElementById("image").innerHTML = 
+       '<img src="' + 
+      imageUrl + 
+      '" alt="Weather image" ' +
+      '>'
+    }
+    else {
+      console.log(err)
+    }
+    
     if (jsonData.main) {
-      //Variable inputs for weather
+      
+      //Variables for weather
       const temp = jsonData.main.temp - 273.15
       const feel = jsonData.main.feels_like - 273.15
       const humidity = jsonData.main.humidity
+      const weather = jsonData.weather[0].description
 
-      //Display weather variables
-      document.getElementById("temperature").innerHTML =
-        "Temperature: " +
-        temp.toFixed(0) +
-        " °C" +
-        "<br>" +
-        "Feels Like: " +
-        feel.toFixed(0) +
-        " °C" +
-        "<br>" +
-        "Humidity: " +
-        humidity.toFixed(0)
-    } else {
-      console.log(error)
-    }
-    //catch error
+      //Output of the variables
+     document.getElementById("temperature").innerHTML = "Temperature: " + temp.toFixed(0) + " °C" + "<br>" + "Feels Like: " + feel.toFixed(0) + " °C" + "<br>" 
+ + "Humidity: " + humidity.toFixed(0) + "<br>" 
+ + "Forecast: " + weather
+  } else {
+    console.log(error)
+  }
+    //error catch
   } catch (err) {
     console.log(err)
   }
 }
 
-getWeather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-)
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
